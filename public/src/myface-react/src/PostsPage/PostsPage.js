b3779b444374437200707bf2b7c569fb90713368
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-10 14:52:17 +0000

    Added 'next' and 'previous' buttons to PostPage.

--- public/src/myface-react/src/PostsPage/PostsPage.js
+++ public/src/myface-react/src/PostsPage/PostsPage.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
-
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function PostsPage() {
 
     const [myData, setMyData] = useState(null);
+    const [page, setPage] = useState(1);
 
     // fetch the data from the backend here (only on first render)
     useEffect(() => {
-        fetch("http://localhost:3001/posts")
+        fetch(`http://localhost:3001/posts/?page=${page}&pageSize=10`)
             .then(response => response.json())
             .then(json => setMyData(json));
-    }, []);
+    }, [page]);
     
     // return it!:
     if (!myData) {
@@ -21,7 +22,7 @@ function PostsPage() {
             <h1>Posts</h1>
             <ol>
             {myData.results.map(function(post, index) {
-                return <li key = {index}> id = {post.id},
+                return <li key = {index}> 
                     Post ID:  {post.id} , 
                     Message:  {post.message} , 
                     Image: {<a href="post.imageUrl" target="_blank">Click here to view image</a>} <br></br>
@@ -31,6 +32,10 @@ function PostsPage() {
                     Dislikes:  {post.dislikedBy.length} 
                 </li>})}               
             </ol>
+   
+            <button onClick={() => myData.next ? setPage(page+1) : setPage(page)}>Next</button>
+            <button onClick={() => myData.previous ? setPage(page-1) : setPage(page)}>Previous</button>
+
         </div>
     );
 }
