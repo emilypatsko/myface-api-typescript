987938c72c47e4fbb0570c27a2d89e30a52d29d3
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-12 10:45:58 +0000

    Created states to keep track of each field in the Create User form.

--- public/src/myface-react/src/CreateUserPage/CreateUserPage.js
+++ public/src/myface-react/src/CreateUserPage/CreateUserPage.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function CreateUserPage() {
+
+    const [name, setName] = useState(null)
+    const [username, setUsername] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [picUrl, setPicUrl] = useState(null);
+    const [coverPicUrl, setCoverPicUrl] = useState(null);
+
     return (
         <div>
             <h1>Create New User</h1>
             <form>
-                Name: <input type = "text" required />
-                Username: <input type = "text" required />
-                Email: <input type = "email" required />
-                Profile picture URL: <input type = "text" required />
-                Cover picture URL: <input type = "text" required />
+                Name: <input type = "text" onInput = {(e) => setName(e.target.value)} required />
+                Username: <input type = "text" onInput = {(e) => setUsername(e.target.value)} required />
+                Email: <input type = "email" onInput = {(e) => setEmail(e.target.value)} required />
+                Profile picture URL: <input type = "text" onInput = {(e) => setPicUrl(e.target.value)} required />
+                Cover picture URL: <input type = "text" onInput = {(e) => setCoverPicUrl(e.target.value)} required />
 
-                <button>Create!</button>
+                <button onClick = {submitForm}>Create!</button>
             </form>
         </div>
     )
+}
+
+const submitForm = (e) => {
+    e.preventDefault();
 }
 
 export {CreateUserPage};
